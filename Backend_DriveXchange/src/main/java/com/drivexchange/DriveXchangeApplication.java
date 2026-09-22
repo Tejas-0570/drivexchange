bf3b5781c9c184aaf40f5a7f50c2a90c46fd1857
Tejas-0570 <waydandetejas111@gmail.com>
2026-09-22 23:43:55 +0530

    Initial Commit: Backend Setup

---
+++ Backend_DriveXchange/src/main/java/com/drivexchange/DriveXchangeApplication.java
@@ -0,0 +1,13 @@
+package com.drivexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveXchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveXchangeApplication.class, args);
+	}
+
+}
---
+++ Backend_DriveXchange/src/test/java/com/drivexchange/DriveXchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drivexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveXchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
